eae9e29a2787114a9b4bdab3cd9ee74b0ffc2f05
Justus Dahunsi <justusdahunsi6@gmail.com>
2022-04-12 20:59:40 +0100

    Portfolio

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,25 @@ import "./works.scss";
 const Works = () => {
   return (
       <div className='works' id="works">
-          
+          <div className="slider">
+            <div className="container">
+              <div className="item">
+                <div className="left">
+                  <div className="leftContainer">
+                    <div className="imageContainer">
+                      <img src="assets/mobile.png" alt="" />
+                    </div>
+                    <h2>Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi sint magnam praesentium debitis minima
+                       corporis, consectetur, repellendus unde cum cumque vitae
+                    </p>
+                    <span>Projects</span>
+                  </div>
+                </div>
+                <div className="right"></div>
+              </div>
+            </div>
+          </div>
       </div>
   );
 }
